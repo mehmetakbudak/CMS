8a7a38bef898a456e6b9074a7dba817cb9e426a9
Mehmet Akbudak <mehmet.akbudak@ecozum.com>
2022-02-07 12:35:13 +0300

    :::

--- CMS.UI/src/views/frontend/Login.vue
+++ CMS.UI/src/views/frontend/Login.vue
@@ -59,7 +59,18 @@
         </div>
         <div class="card" v-if="visibleForgotPassword">
           <div class="card-header py-3">
-            <h4>Şifremi Unuttum</h4>
+            <div class="row">
+              <div class="col-9">
+                <h4>Şifremi Unuttum</h4>
+              </div>
+              <div class="col-3">
+                <Button
+                  @click="showLogin()"
+                  class="p-button-outlined float-end p-sm"
+                  icon="pi pi-arrow-left"
+                />
+              </div>
+            </div>
           </div>
           <div class="card-body">
             <div class="mb-3">
@@ -69,14 +80,6 @@
                 type="email"
                 v-model="forgotPasswordFormData.emailAddress"
               />
-            </div>
-            <div class="p-field">
-              <a
-                class="float-end pb-3 text-decoration-none cursor-pointer"
-                @click="showLogin()"
-              >
-                Giriş Yap
-              </a>
             </div>
             <div class="mb-3">
               <Button
@@ -124,7 +127,7 @@ export default {
       this.exceptions = [];
       this.$store.dispatch("auth/login", this.loginFormData).then(
         () => {
-          location.href="/";
+          location.href = "/";
         },
         (error) => {
           this.loading = false;
